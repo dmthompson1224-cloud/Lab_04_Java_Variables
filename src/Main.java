a58d41c59a88638a969bd12cf6268ded0a9f64fb
dmthompson1224-cloud <dmthompson1224@gmail.com>
2025-09-23 23:10:35 -0400

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,37 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args)
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    {
+
+
+        int intOperandA = 1;
+        int intOperandB = 2;
+        int intSum;
+        int intProduct;
+        int intDifference;
+        int intQuotient;
+        int intModulo;
+
+
+        double doubleOperandA = 3.4;
+        double doubleOperandB = 4.7;
+        double doubleSum;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum);
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleSum);
+
+        double myLunchCost = 12.5;
+        int myRent = 650;
+        boolean Raintoday = false;
+        double TuitionCost = 856.37;
+        boolean Youwin = true;
     }
 }
